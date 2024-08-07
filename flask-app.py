a787ac74af2b5880ebc47284a0f42bad6c66de63
Deepanshu Yadav <Deeepanshu3107@gmail.com>
2024-08-07 13:54:55 +0530

    blog show

--- flask-app.py
+++ flask-app.py
@@ -1,15 +1,32 @@
-from flask import Flask,render_template 
+from flask import Flask,render_template
 app=Flask(__name__)
+
+
+posts=[
+{
+'author':'Deepanshu Yadav',
+'title':'blog post',
+'content':'First Blog Post',
+'date_posted':'April 18 2024'
+},
+{
+'author':'Sam manic',
+'title':'blog post',
+'content':'First Blog Post',
+'date_posted':'August 18 2024'
+}
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-	return render_template("home.html")
+        return render_template("home.html",posts=posts)
 
 
 
 @app.route("/track")
 
 def track():
-	return render_template("track.html")
+        return render_template("track.html")
+
