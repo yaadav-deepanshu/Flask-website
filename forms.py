480feacd6404bc95428b692d172b4d8f0ba1f32a
Deepanshu Yadav <Deeepanshu3107@gmail.com>
2024-08-09 13:41:56 +0530

    regestriation form complete

--- forms.py
+++ forms.py
@@ -1,8 +1,14 @@
 #importing the flask wtForms
 from flask_wtf import FlaskForm
-#importing the string feild 
-from wtforms import StringFeild
-#importing the form validators it is used to validate some parameters like length of the some string like username 
-from wtforms.validators import DataRequired,Length 
+#importing the string feild,password feild and Submit feild
+from wtforms import StringFeild,PasswordFeild,SubmitFeild
+#importing the form validators it is used to validate some parameters like length of the some string like username or email  and equalto
+from wtforms.validators import DataRequired,Length,Email,EqualTo
+
+
 class RegestrationForm(FlaskForm):
 	username=StringField('Username',validators=[DataRequired(),Length(min=2,max=20)] )
+	email=StringField('Email',validators=[DataRequired(),Email()])
+	password =PasswordFeild('Password',validators=[DataRequired()])
+	confirm_password =PasswordFeild('Confirm Password',validators=[DataRequired(),EqualTo('password')])
+	submit=SubmitFeild('Sign Up')
